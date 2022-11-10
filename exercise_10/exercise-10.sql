b017a8076ef54aae292716e137e8124dcc8c36e7
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-10 08:58:27 -0800

    tests 7-11 passing

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,12 @@
 -- find the category name of all inventory items that have never been rented
 -- Similar to exercise 9 with an additonal join
+SELECT
+  category.name
+from
+  category
+  left join film_category on film_category.category_id = category.category_id
+  left join film on film.film_id = film_category.film_id
+  inner join inventory on inventory.film_id = film.film_id
+  left join rental on rental.inventory_id = inventory.inventory_id
+WHERE
+  rental.rental_date IS null
--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,16 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+  film.title,
+  COUNT(rental.rental_id)
+from
+  film
+  left join inventory on inventory.film_id = film.film_id
+  left join rental on rental.inventory_id = inventory.inventory_id
+group by
+  film.film_id
+ORDER by
+  count desc
+LIMIT
+  10
